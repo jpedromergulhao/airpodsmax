6efe8c21243d37f778dc0a920a5d576bc494a125
João Pedro <jpedromergulhao@gmail.com>
2025-01-13 17:33:28 -0300

    Creating the checkout page

--- src/checkout-index.js
+++ src/checkout-index.js
@@ -3,7 +3,6 @@ import ReactDOM from 'react-dom/client';
 import reportWebVitals from './reportWebVitals';
 import Checkout from './Checkout';
 
-
 const root = ReactDOM.createRoot(document.querySelector('#checkout-root'));
 root.render(
   <React.StrictMode>
