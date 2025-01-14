33582138a237c267186c83428a87b042b7c9b5ca
João Pedro <jpedromergulhao@gmail.com>
2025-01-14 00:11:11 -0300

    Rename Components to .jsx to better organization

--- src/checkout-index.jsx
+++ src/checkout-index.jsx
@@ -3,6 +3,8 @@ import ReactDOM from 'react-dom/client';
 import reportWebVitals from './reportWebVitals';
 import Checkout from './Checkout';
 
+console.log("Sucesso!");
+
 const root = ReactDOM.createRoot(document.querySelector('#checkout-root'));
 root.render(
   <React.StrictMode>
